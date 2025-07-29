149dff8bd7815e428b15ef13974e46e9c2b950f2
ychleo102615 <leo102615@gmail.com>
2025-07-30 00:38:40 +0800

    設定輸出配置

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // base: './',
+  base: '/vite-2025-practice/',
   plugins: [
     vue(),
     vueDevTools(),
